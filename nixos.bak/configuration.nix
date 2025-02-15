ffa67bbc078fae659d49d7371cd94b9b1f799bc7
ilianvo <ilianvo@abv.bg>
2025-02-15 14:58:05 +0200

    first commit

---
+++ nixos.bak/configuration.nix
@@ -0,0 +1,192 @@
+# Edit this configuration file to define what should be installed on
+# your system.  Help is available in the configuration.nix(5) man page
+# and in the NixOS manual (accessible by running ‘nixos-help’).
+
+{ config, pkgs, ... }:
+
+{
+  imports =
+    [ # Include the results of the hardware scan.
+       <nixos-hardware/lenovo/thinkpad/t495>
+      ./hardware-configuration.nix
+      ./vm.nix
+    ];
+
+  # Bootloader.
+  boot.loader.systemd-boot.enable = true;
+  boot.loader.efi.canTouchEfiVariables = true;
+
+  networking.hostName = "nixos"; # Define your hostname.
+  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
+
+  # Configure network proxy if necessary
+  # networking.proxy.default = "http://user:password@proxy:port/";
+  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
+
+  # Enable networking
+  networking.networkmanager.enable = true;
+
+  # Set your time zone.
+  time.timeZone = "Europe/Sofia";
+
+  # Select internationalisation properties.
+  i18n.defaultLocale = "en_US.UTF-8";
+
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "bg_BG.UTF-8";
+    LC_IDENTIFICATION = "bg_BG.UTF-8";
+    LC_MEASUREMENT = "bg_BG.UTF-8";
+    LC_MONETARY = "bg_BG.UTF-8";
+    LC_NAME = "bg_BG.UTF-8";
+    LC_NUMERIC = "bg_BG.UTF-8";
+    LC_PAPER = "bg_BG.UTF-8";
+    LC_TELEPHONE = "bg_BG.UTF-8";
+    LC_TIME = "bg_BG.UTF-8";
+  };
+
+  # Enable the X11 windowing system.
+  # You can disable this if you're only using the Wayland session.
+  services.xserver.enable = true;
+
+  # Enable the KDE Plasma Desktop Environment.
+  services.displayManager.sddm.enable = true;
+  services.desktopManager.plasma6.enable = true;
+  services.xserver.desktopManager.gnome.enable = false;
+  services.gnome.gnome-keyring.enable = false;
+
+  services.tlp.enable = false;
+  services.power-profiles-daemon.enable = true;
+
+  services.fprintd.enable = true;
+  services.fprintd.tod.enable = true;
+  services.fprintd.tod.driver = pkgs.libfprint-2-tod1-vfs0090;
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
+  };
+
+  # Enable CUPS to print documents.
+  services.printing.enable = true;
+
+  # Enable sound with pipewire.
+  hardware.pulseaudio.enable = false;
+  security.rtkit.enable = true;
+  services.pipewire = {
+    enable = true;
+    alsa.enable = true;
+    alsa.support32Bit = true;
+    pulse.enable = true;
+    # If you want to use JACK applications, uncomment this
+    #jack.enable = true;
+
+    # use the example session manager (no others are packaged yet so this is enabled by default,
+    # no need to redefine it in your config for now)
+    #media-session.enable = true;
+  };
+
+  # Enable touchpad support (enabled default in most desktopManager).
+  # services.xserver.libinput.enable = true;
+
+  # Define a user account. Don't forget to set a password with ‘passwd’.
+  users.users.hanov = {
+    isNormalUser = true;
+    description = "hanov";
+    extraGroups = [ "networkmanager" "wheel" ];
+    packages = with pkgs; [
+      kdePackages.kate
+    #  thunderbird
+    ];
+  };
+
+  # Install firefox.
+  programs.firefox.enable = true;
+
+  programs.steam.enable = true;
+  programs.steam.gamescopeSession.enable = true;
+  programs.gamemode.enable = true;
+
+programs.git = {
+  config.credential.helper = "manager";
+  config.credential."https://github.com".username = "ilianvo";
+  config.credential.credentialStore = "cache";
+  enable = true;
+};
+
+  # Allow unfree packages
+  nixpkgs.config.allowUnfree = true;
+
+  # List packages installed in system profile. To search, run:
+  # $ nix search wget
+  environment.systemPackages = with pkgs; [
+  #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #  wget
+   pkgs.obsidian
+   pkgs.floorp
+   pkgs.vscodium
+   pkgs.mpv
+   pkgs.qbittorrent
+   pkgs.zoom-us
+   pkgs.syncthing
+   pkgs.telegram-desktop
+   pkgs.exodus
+   pkgs.signal-desktop
+   pkgs.bitwarden-desktop
+   pkgs.woeusb
+   pkgs.kdePackages.plasma-nm
+   pkgs.kdePackages.networkmanager-qt
+   pkgs.kdePackages.modemmanager-qt
+   pkgs.networkmanager
+   pkgs.networkmanagerapplet
+   pkgs.pptp
+   pkgs.ppp
+   pkgs.steam
+   pkgs.github-desktop
+   pkgs.mangohud
+   pkgs.protonup
+   pkgs.mission-center
+   pkgs.openfortivpn
+   pkgs.fprintd
+   pkgs.gh
+   pkgs.git
+   pkgs.fastfetch
+   pkgs.btop
+   pkgs.iftop
+   pkgs.strawberry
+   pkgs.git-credential-manager
+  ];
+ environment.sessionVariables = {
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS =
+      "\${HOME}/.steam/root/compatibilitytools.d";
+};
+  # Some programs need SUID wrappers, can be configured further or are
+  # started in user sessions.
+  # programs.mtr.enable = true;
+  # programs.gnupg.agent = {
+  #   enable = true;
+  #   enableSSHSupport = true;
+  # };
+nix.settings.experimental-features = [ "nix-command" "flakes" ];
+
+  # List services that you want to enable:
+
+  # Enable the OpenSSH daemon.
+  # services.openssh.enable = true;
+
+  # Open ports in the firewall.
+  # networking.firewall.allowedTCPPorts = [ ... ];
+  # networking.firewall.allowedUDPPorts = [ ... ];
+  # Or disable the firewall altogether.
+  # networking.firewall.enable = false;
+
+  # This value determines the NixOS release from which the default
+  # settings for stateful data, like file locations and database versions
+  # on your system were taken. It‘s perfectly fine and recommended to leave
+  # this value at the release version of the first install of this system.
+  # Before changing this value read the documentation for this option
+  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
+  system.stateVersion = "24.11"; # Did you read the comment?
+
+}
+
