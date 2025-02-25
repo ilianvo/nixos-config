199aa5650243361c91e4a36139ad6b814d059e06
ilianvo <ilianvo@abv.bg>
2025-02-25 20:54:00 +0200

    openssh

--- configuration.nix
+++ configuration.nix
@@ -173,7 +173,7 @@ nix.settings.experimental-features = [ "nix-command" "flakes" ];
   # List services that you want to enable:
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+   services.openssh.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
